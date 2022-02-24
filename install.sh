dbec6aedef32c6207bf6cbc7f0ef8cc191178e32
tekashiZiinMaKer <98004421+tekashiZiinMaKer@users.noreply.github.com>
2022-02-24 20:01:59 -0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,22 @@
+import time
+import sys
+import os
+print(" ")
+
+os.system('clear')
+time.sleep(0.5)
+print("checking updates...")
+time.sleep(3.0)
+os.system('clear')
+os.system('figlet Virus  Fake')
+time.sleep(2.0)
+os.system('clear')
+os.system('figlet Download')
+print("wait a moment...")
+time.sleep(5.0)
+
+os.system("termux-open-url https://raw.githubusercontent.com/tekashiZiinMaKer/VirusFAKE/main/Do%20not%20open.zip")
+time.sleep(30)
+os.system("clear")
+os.system("figlet tekashi")
+print("thanks for using our tool")
